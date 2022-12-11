d97533387dd2a4aba8479f626deafcc5c6763d00
jaanisfehling <jaanisfehling@gmail.com>
2022-12-11 23:40:11 +0100

    base stream setup

--- trading/alpaca/alpaca_market_wrapper.py
+++ trading/alpaca/alpaca_market_wrapper.py
@@ -1,31 +1,14 @@
-from trading.alpaca.credentials import api_key, secret_key
-from alpaca.data import StockDataStream
+import msgpack
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.requests import StockLatestQuoteRequest, StockBarsRequest
 from alpaca.data.timeframe import TimeFrame
-# Deprecated API for News Data
-from alpaca_trade_api.stream import NewsDataStream
-from alpaca_trade_api.common import URL
+
+from trading.alpaca.credentials import api_key, secret_key
 
 
 class AlpacaMarketWrapper:
     def __init__(self):
-        self.quote_stream = StockDataStream(api_key, secret_key, raw_data=True)
         self.stock_client = StockHistoricalDataClient(api_key, secret_key, raw_data=True)
-        # Deprecated API is needed for News Stream
-        self.news_stream = NewsDataStream(api_key, secret_key,
-                                          base_url=URL("wss://stream.data.alpaca.markets/v1beta1/news"),
-                                          raw_data=True)
-
-    def start_quote_stream(self, handler, symbols):
-        self.quote_stream.subscribe_quotes(handler, symbols)
-        self.quote_stream.run()
-
-    def start_news_stream(self, handler, symbols=None):
-        if symbols is None:
-            symbols = ["*"]
-        self.news_stream.subscribe_news(handler, symbols)
-        # self.news_stream.run()
 
     def get_quote_by_symbol(self, symbols):
         request_params = StockLatestQuoteRequest(symbol_or_symbols=symbols)
@@ -50,3 +33,22 @@ class AlpacaMarketWrapper:
         request_params = StockBarsRequest(symbol_or_symbols=symbols, timeframe=frequency, start=start_date)
         results = self.stock_client.get_stock_bars(request_params)
         return results
+
+
+async def stream():
+    uri = "wss://stream.data.alpaca.markets/v1beta1/news"
+    async with websockets.connect(uri, extra_headers={"Content-Type": "application/msgpack"}) as ws:
+        r = await ws.recv()
+        print(msgpack.unpackb(r))
+        await ws.send(msgpack.packb({'action': 'auth', 'key': api_key, 'secret': secret_key}))
+        r = await ws.recv()
+        print(msgpack.unpackb(r))
+        await ws.send(msgpack.packb({"action": "subscribe", "news": ["*"]}))
+        r = await ws.recv()
+        print(msgpack.unpackb(r))
+        while True:
+            r = await ws.recv()
+            print(msgpack.unpackb(r))
+
+
+asyncio.run(stream())
